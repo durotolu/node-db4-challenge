f118dad8dae7601db737730637785d953ee9a33e
Modurotolu Olokode <durolawk@gmail.com>
2019-11-07 22:05:09 +0100

    created getInstructors and getshoppinglist

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -2,14 +2,25 @@ const db = require('../data/db-config')
 
 module.exports = {
     getRecipes,
-    //getShoppingList,
-    //getInstructions
+    getShoppingList,
+    getInstructions
 }
 
 function getRecipes() {
     return db('recipes')
 }
 
-//function getShoppingList(recipe_id) {}
+function getShoppingList(recipe_id) {
+    return db('recipes as r')
+        .join('recipesIngredients as ri', 'r.id', 'ri.recipesId')
+        .join('ingredients as i', 'ri.ingredientsId', 'i.id')
+        .select('r.mealName', 'i.ingredientName', 'ri.ingredientQuantity')
+        .where({ 'r.id': recipe_id });
+}
 
-//function getInstructions(recipe_id) {}
+function getInstructions(recipe_id) {
+    return db('recipes as r')
+        .join('steps as s', 'r.id', 's.recipesId')
+        .select('r.mealName', 's.step', 's.stepsNumber')
+        .where({ 'r.id': recipe_id });
+}
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -13,5 +13,37 @@ router.get('/', (req, res) => {
       res.status(500).json({ 'Failed to get recipes': err.message });
     });
   });
+
+  router.get('/:id/list', (req, res) => {
+    const { id } = req.params;
+  
+    Recipes.getShoppingList(id)
+    .then(lists => {
+      if (lists.length) {
+        res.json(lists);
+      } else {
+        res.status(404).json({ 'Could not find lists for given recipe': err.message })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ 'Failed to get list': err.message });
+    });
+  });
+
+  router.get('/:id/instructions', (req, res) => {
+    const { id } = req.params;
+  
+    Recipes.getInstructions(id)
+    .then(instructions => {
+      if (instructions.length) {
+        res.json(instructions);
+      } else {
+        res.status(404).json({ 'Could not find instructions for given recipe': err.message })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ 'Failed to get list': err.message });
+    });
+  });
   
   module.exports = router;
